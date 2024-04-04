e186b6c76bda253a968cfdedf517db186f475b71
j1guzman <j1guzman@fundaciongruposocial.co>
2024-04-03 21:25:01 -0500

    feat: add .env

--- src/main.go
+++ src/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	App(mux)
 
-	fmt.Println("The server is listening on the port http://localhost:8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	fmt.Println("The server is listening on the port http://localhost" + ":" + port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
 		fmt.Printf("Error starting server: %v", err)
 	}
 }
