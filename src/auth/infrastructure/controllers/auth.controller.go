e9eeb2d146b3cc8b7cf980eb6623beb1b704a037
j1guzman <j1guzman@fundaciongruposocial.co>
2024-04-10 21:56:35 -0500

    feat: commons

--- src/auth/infrastructure/controllers/auth.controller.go
+++ src/auth/infrastructure/controllers/auth.controller.go
@@ -3,16 +3,14 @@ package controllers
 import (
 	"brujulavirtual-auth/src/auth/domain/models"
 	"brujulavirtual-auth/src/auth/domain/ports"
+	"brujulavirtual-auth/src/commons"
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
 type Controller struct {
 	service ports.Service
-}
-
-type ErrorResponse struct {
-	Error string `json:"error"`
 }
 
 func AuthController(service ports.Service) *Controller {
@@ -31,29 +29,21 @@ func (c *Controller) Validate(w http.ResponseWriter, r *http.Request) {
 func (c *Controller) ValidatePost(w http.ResponseWriter, r *http.Request) {
 
 	var auth models.Auth
-
 	err := json.NewDecoder(r.Body).Decode(&auth)
+	log.Default().Println(auth)
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		errorResponse := ErrorResponse{
-			Error: "Error processing data",
-		}
-		err := json.NewEncoder(w).Encode(errorResponse)
-		if err != nil {
-			return
-		}
+		commons.ErrorResponse(w, "Error processing data", http.StatusBadRequest)
 		return
 	}
 
 	if !auth.IsValid() {
-		http.Error(w, "Incomplete or invalid authentication data", http.StatusBadRequest)
+		commons.ErrorResponse(w, "Incomplete or invalid authentication data", http.StatusBadRequest)
 		return
 	}
 
 	createdAuth, err := c.service.Validate(auth)
 	if err != nil {
-		http.Error(w, "Error creating authenticator entity", http.StatusInternalServerError)
+		commons.ErrorResponse(w, "Error creating authenticator entity", http.StatusInternalServerError)
 		return
 	}
 
@@ -61,7 +51,7 @@ func (c *Controller) ValidatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	err = json.NewEncoder(w).Encode(createdAuth)
 	if err != nil {
-		return
+		log.Default().Printf("Error encoding the response: %v", err)
 	}
 
 }
